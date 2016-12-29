ba8a285efa02dbc9ef8a77e89153696c4cabf1dc
Filipe de Campos Cavalcante <filipe.cavalcante@programmers.com.br>
2016-12-29 10:13:44 -0200

    added UnitTests

---
+++ PI.Framework.SharePoint/PI.Framework.SharePoint.Tests/Entity/TaskEntity.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.SharePoint;
+using PI.Framework.SharePoint.Base;
+using PI.Framework.SharePoint.Mapper;
+
+namespace PI.Framework.SharePoint.Tests.Entity
+{
+    [ListMapper(ListTemplate = SPListTemplateType.Tasks, ListTitle = "Tasks", RelativeUrl = "/")]
+    public class TaskEntity : SPBaseEntity
+    {
+        [FieldMapper(InternalName = "Status")]
+        public string StatusTarefa { get; set; }
+        public SPFieldUserValue AssignedTo { get; set; }
+    }
+}
